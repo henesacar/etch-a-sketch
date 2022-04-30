5dfbb3c2e31962fe38fec88096e42c33d1dbe381
Enes Acar <h.enesacar@hotmail.com>
2022-04-30 20:54:36 +0300

    Solved color problem

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const content = document.getElementById("container");
 
-function makeRows(rowN,colN){
-    content.style.setProperty("--grid-rows",rowN);
-    content.style.setProperty("--grid-cols",colN);
-    for(i = 0; i < (rowN*colN); i++){
-        let cell = document.createElement("div");
-        cell.innerText = (i+1);
-        content.appendChild(cell).className = "grid-item";
-    }
+function makeRows(rowN, colN) {
+  content.style.setProperty("--grid-rows", rowN);
+  content.style.setProperty("--grid-cols", colN);
+  for (let i = 0; i < rowN * colN; i++) {
+    let cell = document.createElement("div");
+    cell.innerText = i + 1;
+    content.appendChild(cell).className = "grid-item";
+  }
 }
-makeRows(16,16);
+makeRows(16, 16);
 const grid = document.getElementsByClassName("grid-item");
 
-function changeColor(){
-    for(let i = 0;i < grid.length;i++){
-        grid[i].classList.toggle("change");
-    }
+function changeColor(index) {
+  grid[index].classList.toggle("change");
 }
 
-for(let i = 0;i < grid.length;i++){
-    grid[i].addEventListener("mouseenter",changeColor);
-    //grid[i].addEventListener("mouseover",changeColor);
+for (let i = 0; i < grid.length; i++) {
+  grid[i].addEventListener("mouseenter", changeColor.bind(null, i));
+  //grid[i].addEventListener("mouseover",changeColor);
 }
